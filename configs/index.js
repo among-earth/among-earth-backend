a9b8e246ba8ab391af87fe886983d2cd27f58577
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-12-31 17:22:06 +0900

    :recycle: refactor code

--- configs/index.js
+++ configs/index.js
@@ -5,11 +5,11 @@ module.exports = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
   },
-  databaseURL: process.env.MONGODB_URL,
-  mongooseOptions: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    dbName: 'sool-tok',
-  },
+  // databaseURL: process.env.MONGODB_URL,
+  // mongooseOptions: {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false,
+  //   dbName: 'sool-tok',
+  // },
 };
