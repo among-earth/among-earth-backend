76453efe1dce5083506b0f6f90011e380ad08e9d
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-20 00:14:27 +0900

    :rotating_light: fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      mocha: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    rules: {
-      'comma-dangle': ['warn', 'always-multiline'],
-      'arrow-parens': ['warn', 'as-needed'],
-      'eol-last': ['warn', 'always'],
-      'no-unused-vars': ['warn', { args: 'none' }],
-      quotes: ['warn', 'single'],
-      semi: ['warn', 'always'],
-      'space-before-function-paren': [
-        'warn',
-        {
-          anonymous: 'always',
-          named: 'never',
-          asyncArrow: 'always',
-        },
-      ],
-      'space-before-blocks': ['warn', 'always'],
-      'no-process-env': ['warn', 'always'],
-    },
-  };
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    'arrow-parens': ['warn', 'as-needed'],
+    'eol-last': ['warn', 'always'],
+    'no-unused-vars': ['warn', { args: 'none' }],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'space-before-function-paren': [
+      'warn',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'space-before-blocks': ['warn', 'always'],
+  },
+};
